fb41179162c50125bf7f6216fb375e009e668f80
zidniryi <zidnirydwan@gmail.com>
2021-09-21 21:38:04 +0700

    Start Project

---
+++ README.md
@@ -0,0 +1,22 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
+</p>
+
+## Cara Installasi
+
+- Buka Terminal / CMD
+- Clone project <code>https://github.com/wildanfuady/Simple-CRUD-Laravel-8.git</code>
+- cd simple-crud-laravel-8
+- Ketik <code>composer install</code>
+- Kemudian ketik <code>cp .env.example .env</code>
+- Ketik <code>php artisan key:generate</code>
+- Buat database baru bernama <code>db_laravel8_crud</code>
+- Konfigurasi database di <code>.env</code>
+- Ketik <code>php artisan migrate</code>
+- Jalankan dengan <code>php artisan serve</code>
+- Buka browser dan ketik <code>http://127.0.0.1:8000/posts</code>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create New Post</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('posts.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Task Name:</strong>
+                <input type="text" name="title" class="form-control" placeholder="Title">
+            </div>
+        </div>
+      <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NRP:</strong>
+                <input type="text" name="nrp" class="form-control" placeholder="Title">
+            </div>
+        </div>
+      <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="Title">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Keterangan Mulai dan Kapan Selesai:</strong>
+                <textarea class="form-control" style="height:150px" name="content" placeholder="Content"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-success block">Tambah Task</button>
+        </div>
+    </div>
+
+</form>
+@endsection
